a77c9b7c83adca0fd60c3e32d4804c4bc5aba425
konsatanasoff <konsatanasoff@gmail.com>
2021-03-06 20:42:09 +0200

    commit

---
+++ attributes_and_methods/lab/integer.py
@@ -0,0 +1,19 @@
+class Integer:
+    def __init__(self, value):
+        self.value = value
+
+    @classmethod
+    def from_float(cls, value):
+        pass
+
+    @classmethod
+    def from_roman(cls, value):
+        pass
+
+    @classmethod
+    def from_string(cls, value):
+        pass
+
+    @classmethod
+    def add_integer(cls, integer):
+        pass
