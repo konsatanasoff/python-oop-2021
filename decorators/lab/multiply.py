226277d3def9d4cd44a80e165f4235b9b0bae65e
konsatanasoff <konsatanasoff@gmail.com>
2021-04-02 21:51:25 +0300

    lab optimization

--- decorators/lab/multiply.py
+++ decorators/lab/multiply.py
@@ -1,5 +1,9 @@
+import functools
+
+
 def multiply(times):
     def decorator(func):
+        @functools.wraps(func)
         def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
             return result * times
