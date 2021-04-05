40b12e000ccb02cd1ed1fd59566a3d2fff9765ef
konsatanasoff <konsatanasoff@gmail.com>
2021-04-05 14:55:13 +0300

    decorators final commit

--- decorators/exercises/cache.py
+++ decorators/exercises/cache.py
@@ -1,19 +1,23 @@
 def cache(func):
-    # TODO: Implement
-
-    @cache
-    def fibonacci(n):
-
-    if n < 2:
-
-        return n
-
-    else:
-
-        return fibonacci(n - 1) + fibonacci(n - 2)
+    def wrapper(n):
+        result = func(n)
+        wrapper.log[n] = result
+        return result
+    wrapper.log = {}
+    return wrapper
 
 
-fibonacci(3)
-print(fibonacci.log)
-fibonacci(4)
-print(fibonacci.log)
+# @cache
+# def fibonacci(n):
+#     if n < 2:
+#         return n
+#
+#     else:
+#
+#         return fibonacci(n - 1) + fibonacci(n - 2)
+#
+#
+# fibonacci(3)
+# print(fibonacci.log)
+# fibonacci(4)
+# print(fibonacci.log)
